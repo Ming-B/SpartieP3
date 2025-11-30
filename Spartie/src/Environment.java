61daf0e6f4d38999cd0d155bb4009d1cb146c4ef
Ming Bian <mynameisking67@gmail.com>
2025-11-30 11:07:49 -0500

    modified Environment to assign and get variables, fixed line issue on Windows machine for SpartieScanner

--- Spartie/src/Environment.java
+++ Spartie/src/Environment.java
@@ -19,20 +19,27 @@ public class Environment {
     }
 
     Object get(String name) {
-        // TODO: Return variable if it exists in our current environment, otherwise, check enclosing, otherwise,
-        //  return null (it does not exist)
-
-        return null;
+        if(variables.containsKey(name)) {
+            return variables.get(name);
+        }
+        else if(enclosing != null) { //checks enclosing if variable is not in table
+            return enclosing.variables.get(name);
+        }
+        else return null;
     }
 
     // Assign - Replace the value of an existing variable
     void assign(Token name, Object value) {
-        // TODO: If the variable exists, then we can assign, otherwise we have an error
-
-        // TODO: If we don't have it in our current environment, try assigning in the enclosing environment
-
-        // Exit on error if we get this far since the variable is undefined
-        System.err.println("Undefined variable: " + name.text);
-        System.exit(ErrorCode.INTERPRET_ERROR);
+        if(variables.containsKey(name.text)) {
+            variables.put(name.text, value);
+        }
+        else if(enclosing != null) {
+            enclosing.assign(name, value);
+        }
+        else {
+            // Exit on error if we get this far since the variable is undefined
+            System.err.println("Undefined variable: " + name.text);
+            System.exit(ErrorCode.INTERPRET_ERROR);
+        }
     }
 }
